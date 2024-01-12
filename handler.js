6fe1c0ebb2668fdaaca5b0f7be7da09f4bb93d26
alihassan826 <alihassan486788@gmail.com>
2024-01-12 22:25:10 +0000

    aws lambda crud api done

---
+++ handler.js
@@ -0,0 +1,94 @@
+'use strict';
+
+const mongoose = require('mongoose');
+
+mongoose.connect('DB_URL')
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  email: {
+    type: String
+  }
+});
+
+const User = mongoose.model('user', userSchema);
+
+module.exports.create = async (event) => {
+  const data = JSON.parse(event.body);
+
+  try {
+    const newUser = await User.create(data);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(newUser)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: error })
+    };
+  }
+};
+
+
+module.exports.read = async (event) => {
+  try {
+    const users = await User.find();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(users)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: error })
+    };
+  }
+};
+
+
+module.exports.update = async (event) => {
+  const { id } = event.pathParameters;
+  const data = JSON.parse(event.body);
+
+  try {
+    const users = await User.findOneAndUpdate({
+      _id: id
+    }, {
+      $set: { data }
+    }, { new: true });
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(users)
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: error })
+    };
+  }
+};
+
+
+module.exports.delete = async (event) => {
+  const { id } = event.pathParameters;
+
+  try {
+    await User.findByIdAndDelete(id);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'User deleted Succesfully' })
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: error })
+    };
+  }
+};
